b92567107551f182abf8407e8ac2586d37e0082c
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2022-10-19 15:54:18 +0200

    remove unprepared geometries in intersects

--- geos/intersects.R
+++ geos/intersects.R
@@ -6,27 +6,14 @@ points = as_geos_geometry(points)
 polygon = read_sf("data/polygon.gpkg")
 polygon = as_geos_geometry(polygon)
 
-###############################################
-### geometry is not prepared
-t_vec_1 = numeric(10)
-for (i in seq_len(10)) {
-
-  t = system.time(geos_intersects(points, polygon))
-  t_vec_1[i] = t[["elapsed"]]
-
-}
-
-###############################################
-### geometry is prepared
-t_vec_2 = numeric(10)
+t_vec = numeric(10)
 for (i in seq_len(10)) {
 
   t = system.time(geos_prepared_intersects(points, polygon))
-  t_vec_2[i] = t[["elapsed"]]
+  t_vec = t[["elapsed"]]
 
 }
 
-output = rbind(data.frame(task = "intersects", package = "geos", time = t_vec_1),
-               data.frame(task = "intersects", package = "geos-prepared", time = t_vec_2))
+output = rbind(data.frame(task = "intersects", package = "geos", time = t_vec))
 if (!dir.exists("results")) dir.create("results")
 write.csv2(output, "results/intersects-geos.csv", row.names = FALSE)
--- sf/intersects.R
+++ sf/intersects.R
@@ -3,27 +3,14 @@ library(sf)
 points = read_sf("data/points.gpkg")
 polygon = read_sf("data/polygon.gpkg")
 
-###############################################
-### geometry is not prepared
-t_vec_1 = numeric(10)
-for (i in seq_len(10)) {
-
-  t = system.time(st_intersects(points, polygon, sparse = FALSE, prepared = FALSE))
-  t_vec_1[i] = t[["elapsed"]]
-
-}
-
-###############################################
-### geometry is prepared
-t_vec_2 = numeric(10)
+t_vec = numeric(10)
 for (i in seq_len(10)) {
 
   t = system.time(st_intersects(points, polygon, sparse = FALSE, prepared = TRUE))
-  t_vec_2[i] = t[["elapsed"]]
+  t_vec[i] = t[["elapsed"]]
 
 }
 
-output = rbind(data.frame(task = "intersects", package = "sf", time = t_vec_1),
-               data.frame(task = "intersects", package = "sf-prepared", time = t_vec_2))
+output = rbind(data.frame(task = "intersects", package = "sf", time = t_vec))
 if (!dir.exists("results")) dir.create("results")
 write.csv2(output, "results/intersects-sf.csv", row.names = FALSE)

815a94c5fdc8bb0b26aae205570f97e5569b4efc
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-04-07 13:04:17 -0400

    Implement `buffer` via LibGEOS integration in GeometryOps
    This is currently an active PR on GeometryOps but will be merged this week.

---
+++ geometryops/buffer.jl
@@ -0,0 +1,25 @@
+import GeometryOps as GO, GeoInterface as GI
+using CairoMakie
+import GeoDataFrames # easiest package to use to load Geopackage files
+using Chairmarks
+
+using LibGEOS # activate buffer capabilities in GeometryOps
+
+include("utils.jl")
+
+# Load the data
+data_path = joinpath(dirname(@__DIR__), "data")
+points_gpkg = GeoDataFrames.read(joinpath(data_path, "points.gpkg"))
+polygon_gpkg = GeoDataFrames.read(joinpath(data_path, "polygon.gpkg"))
+# Process it into a Julia form
+point_set = map(points_gpkg.geom) do row
+    Point2d(GI.x(row), GI.y(row))
+end
+polygon = GO.apply(p -> Point2d(GI.x(p), GI.y(p)), GI.PointTrait(), only(polygon_gpkg.geom))
+# Benchmark the distance function
+# This uses the `Chairmarks.jl` package to benchmark the `GO.distance` function.
+# The benchmark will run for 15 seconds.
+benchmark = @be GO.buffer.((GO.GEOS(),), point_set, 100) seconds=15
+
+# Write the results to a CSV file
+write_benchmark_as_csv(benchmark; task = "buffer")

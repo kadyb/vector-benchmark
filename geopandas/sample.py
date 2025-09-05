e6b358ee96d073c0def86d0da3eed9abe033805b
Maarten Pronk <git@evetion.nl>
2025-09-05 10:28:13 +0200

    Add Python environment with sample_points.

--- geopandas/sample.py
+++ geopandas/sample.py
@@ -8,29 +8,12 @@ wd = os.getcwd()
 vec = os.path.join(wd, "data", "polygon.gpkg")
 gdf = geopandas.read_file(vec)
 
-def sample(polygon, size):
-    count = 0
-    points = []
-    x_min, y_min, x_max, y_max = polygon.total_bounds
-
-    while count < size:
-        pts = geopandas.GeoSeries.from_xy(
-            numpy.random.uniform(x_min, x_max, size),
-            numpy.random.uniform(y_min, y_max, size)
-        )
-        hits = pts[pts.sindex.query(polygon.geometry.iloc[0], predicate = "contains")]
-        count += len(hits)
-        points.extend(hits)
-
-    gdf = geopandas.GeoDataFrame(geometry = points[0:size], crs = polygon.crs)
-    return gdf
-
 n = 100000
 t_list = [None] * 10
 for i in range(10):
     tic = timeit.default_timer()
 
-    smp = sample(gdf, n)
+    smp = gdf.sample_points(n)
 
     toc = timeit.default_timer()
     t_list[i] = round(toc - tic, 2)
